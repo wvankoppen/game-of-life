9397a7a439672c6bfa349c6f95a15148bd30a5b6
Wouter van Koppen <wvkoppen@gmail.com>
2022-11-01 20:38:24 +0100

    Create PWA

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { GameOfLifeModule } from './game-of-life/game-of-life.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, FormsModule, GameOfLifeModule],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        GameOfLifeModule,
+        ServiceWorkerModule.register('ngsw-worker.js', {
+            enabled: environment.production,
+            // Register the ServiceWorker as soon as the application is stable
+            // or after 30 seconds (whichever comes first).
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+    ],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
