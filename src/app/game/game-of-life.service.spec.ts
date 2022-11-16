a6f6142bb7709609aa83e4801222092bc8ea5ff0
Wouter van Koppen <wvkoppen@gmail.com>
2022-11-16 19:54:50 +0100

    fix unit test

--- src/app/game/game-of-life.service.spec.ts
+++ src/app/game/game-of-life.service.spec.ts
@@ -1,31 +1,17 @@
-// describe('GameOfLife', () => {
-//     it('creates a world with correct dimensions', () => {
-//         const world = new World(1, 2);
-//         expect(world.cols).toBe(1);
-//         expect(world.rows).toBe(2);
-//     });
-//
-//     it('creates an empty world', () => {
-//         const world = new World(1, 2);
-//         expect(world.livingCellCount).toBe(0);
-//     });
-//
-//     it('toggles a cell', () => {
-//         const world = new World(3, 3);
-//         const center = { col: 1, row: 1 };
-//
-//         world.toggle(center);
-//
-//         expect(world.livingCellCount).toBe(1);
-//     });
-//
-//     it('living cell dies', () => {
-//         const world = new World(3, 3);
-//         const center = { col: 1, row: 1 };
-//
-//         world.toggle(center);
-//         world.evolve();
-//
-//         expect(world.livingCellCount).toBe(0);
-//     });
-// });
+import { firstValueFrom } from 'rxjs';
+import { GameOfLifeService } from './game-of-life.service';
+
+describe('GameOfLifeService', () => {
+    let sut: GameOfLifeService;
+
+    beforeEach(() => {
+        sut = new GameOfLifeService();
+    });
+
+    it('creates a world with correct dimensions', async () => {
+        sut.resize(1, 2);
+        const world = await firstValueFrom(sut.evolution$);
+        expect(world.cells.length).toBe(1);
+        expect(world.cells.length).toBe(2);
+    });
+});
